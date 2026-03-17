345b502eeab2c9d7fcc899c85af7054434ff52d8
Cameron Stewart <cg@rocktownlabs.com>
2026-03-17 00:09:10 -0500

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,47 @@
+import { expo } from "@better-auth/expo";
+import { db } from "@fresh-mansions/db";
+import * as schema from "@fresh-mansions/db/schema/auth";
+import { env } from "@fresh-mansions/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "sqlite",
+
+    schema: schema,
+  }),
+  trustedOrigins: [
+    env.CORS_ORIGIN,
+    "fresh-mansions://",
+    ...(env.NODE_ENV === "development"
+      ? ["exp://", "exp://**", "exp://192.168.*.*:*/**", "http://localhost:8081"]
+      : []),
+  ],
+  emailAndPassword: {
+    enabled: true,
+  },
+  // uncomment cookieCache setting when ready to deploy to Cloudflare using *.workers.dev domains
+  // session: {
+  //   cookieCache: {
+  //     enabled: true,
+  //     maxAge: 60,
+  //   },
+  // },
+  secret: env.BETTER_AUTH_SECRET,
+  baseURL: env.BETTER_AUTH_URL,
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+    // uncomment crossSubDomainCookies setting when ready to deploy and replace <your-workers-subdomain> with your actual workers subdomain
+    // https://developers.cloudflare.com/workers/wrangler/configuration/#workersdev
+    // crossSubDomainCookies: {
+    //   enabled: true,
+    //   domain: "<your-workers-subdomain>",
+    // },
+  },
+  plugins: [expo()],
+});
