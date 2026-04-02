5a27e334d9062b9d241ec0aaa455f9ee4c767cca
Cameron Stewart <cg@rocktownlabs.com>
2026-04-02 04:22:38 -0500

    Fix Sentry browser init in worker build

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -26,7 +26,7 @@ export const getRouter = () => {
     scrollRestoration: true,
   });
 
-  if (!router.isServer && !Sentry.getClient()) {
+  if (typeof window !== "undefined" && !Sentry.getClient()) {
     Sentry.init({
       dsn: "https://9a87030bdd0746fba8ced39c2bc07ae1@o4510278858309632.ingest.us.sentry.io/4511149589725184",
       enableLogs: true,
