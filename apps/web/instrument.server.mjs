b9af17e8548873c25ccd7b78d77598ad4ffa445e
Cameron Stewart <cg@rocktownlabs.com>
2026-04-02 04:24:51 -0500

    Fix Sentry web setup for Cloudflare workers

--- apps/web/instrument.server.mjs
+++
@@ -1,8 +0,0 @@
-import * as Sentry from "@sentry/tanstackstart-react";
-
-Sentry.init({
-  dsn: "https://9a87030bdd0746fba8ced39c2bc07ae1@o4510278858309632.ingest.us.sentry.io/4511149589725184",
-  enableLogs: true,
-  sendDefaultPii: true,
-  tracesSampleRate: 1,
-});
